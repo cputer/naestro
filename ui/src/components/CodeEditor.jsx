e8d5729415789b20daefc07955b7e96b3bc9550a
Nikolai Nedovodin <info@cputer.com>
2025-09-02 06:03:27 -0700

    feat(ui): add prop types to CodeEditor

--- ui/src/components/CodeEditor.jsx
+++ ui/src/components/CodeEditor.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Editor, DiffEditor } from '@monaco-editor/react';
 
-const CodeEditor = ({ original = '', modified, language = 'javascript', height = '400px' }) => {
-  if (modified !== undefined) {
+const CodeEditor = ({ original, modified, language = 'javascript', height }) => {
+  if (modified) {
     return (
       <DiffEditor
         original={original}
@@ -22,6 +23,18 @@ const CodeEditor = ({ original = '', modified, language = 'javascript', height =
       options={{ automaticLayout: true }}
     />
   );
+};
+
+CodeEditor.propTypes = {
+  language: PropTypes.string.isRequired,
+  original: PropTypes.string,
+  modified: PropTypes.string,
+  height: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
+};
+CodeEditor.defaultProps = {
+  original: '',
+  modified: '',
+  height: 400
 };
 
 export default CodeEditor;
