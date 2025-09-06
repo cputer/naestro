7b21eb91c2decaddda6fd563905159e806c9edec
Nikolai Nedovodin <info@cputer.com>
2025-09-05 18:18:31 -0700

    chore: use swc react plugin and include svg assets

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ["**/*.svg"],
   server: {
     host: true,
     port: 5173,
