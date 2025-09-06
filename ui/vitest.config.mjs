91d56ead758d2805381a3a416b5fccae806a52ce
Nikolai Nedovodin <info@cputer.com>
2025-09-05 20:18:37 -0700

    chore(ui): alias jest-dom for vitest

--- ui/vitest.config.mjs
+++ ui/vitest.config.mjs
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: /^@testing-library\/jest-dom$/, replacement: '@testing-library/jest-dom/vitest' },
+    ],
+  },
   test: {
     globals: true,
     environment: 'jsdom',
